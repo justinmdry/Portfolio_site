f7b435ca79aac51a8f44300dc4320ca44fc677d1
justinmdry <justintime200252@gmail.com>
2025-05-07 13:07:59 -0400

    Attempt at fixing an issue on mobile with screen sizing.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import TimeLine from './components/TimeLine';
 // Colors :rgb(34, 34, 34), #1a1a1a
 function App(){
   return( 
-    <div className="d-flex flex-column min-vw-100 align-items-center justify-content-between text-white" style={{ backgroundColor: 'rgb(34, 34, 34)' }}>
+    <div className="d-flex flex-column sm:w-100 align-items-center justify-content-between text-white" style={{ backgroundColor: 'rgb(34, 34, 34)' }}>
       <div className="d-flex flex-column flex-wrap align-items-center justify-content-center mt-4 mb-5 p-3 w-100" style={{ maxWidth: '650px' }}>
         <div className="d-flex flex-column w-100 mb-4">
           <div className="mt-4 font-monospace">
